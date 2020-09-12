34142a30e3b969dc41b9fd9d83ba97fb3621a491
Glitch (minaabespy) <none>
2020-09-12 16:33:26 +0000

    🐵👁 Updated with Glitch

--- MinaPort/js/main.js
+++ MinaPort/js/main.js
@@ -1,4 +1,6 @@
 let gap = 0;
+let gapV = 0;
+var link;
 $(document).ready(function() //When the page is ready, load function
 {
   let isMobile;
@@ -8,22 +10,51 @@ $(document).ready(function() //When the page is ready, load function
     isMobile = true;
   }
   if (isMobile == false) {
+    $(".uClick").on("click", function() {
+       
+      let splitted = link.split("-");
+      let numberLink = splitted[1];
+       numberLink= numberLink-1;
+       
+      let destination = splitted[0] + "-" + numberLink;
+     destination = $(destination).attr("href");
+      console.log(link, destination);
+      var speed = 750; // Durée de l'animation (en ms)
+      $("html, body").animate({ scrollTop: $(destination).offset().top }, speed); // Go
+      return false;
+    });
+    
+     $(".dClick").on("click", function() {
+     
+      let splitted = link.split("-");
+     let numberLink2= splitted[1];
+       numberLink2= numberLink2-(-1);
+       
+        console.log("link: "+link+" "+"splitted: "+splitted+"numberLink2: "+numberLink2);
+     
+     let destination = splitted[0] + "-" + numberLink2;
+     destination = $(destination).attr("href");
+      var speed = 750; // Durée de l'animation (en ms)
+      $("html, body").animate({ scrollTop: $(destination).offset().top }, speed); // Go
+      return false;
+    });
     $(".pageDetector ").hover(
       function() {
-        var link = $(this).attr("href");
+        
+        link = $(this).attr("href");
         $(link).addClass("pageOver");
 
         // debut crantage du scroll
-        
-       // var page = "#" + $(this).attr("id"); // Page cible
-       // var speed = 750; // Durée de l'animation (en ms)
-       // $("html, body").animate({ scrollTop: $(page).offset().top }, speed); // Go
-       // return false;
-        
+
+        // var page = "#" + $(this).attr("id"); // Page cible
+        // var speed = 750; // Durée de l'animation (en ms)
+        // $("html, body").animate({ scrollTop: $(page).offset().top }, speed); // Go
+        // return false;
+
         // fin crantage du scroll
       },
       function() {
-        var link = $(this).attr("href");
+        link = $(this).attr("href");
         $(link).removeClass("pageOver");
       }
     );
@@ -96,75 +127,54 @@ $(document).ready(function() //When the page is ready, load function
   $("a:-webkit-any-link.carousel-control-next").mouseout(function() {
     gap = 0;
   });
+  $(".verticalControlUp").mouseenter(function() {
+    gapV = -5;
+    gap = 0.1;
+    // console.log(gapV);
+  });
+  $(".verticalControlUp").mouseout(function() {
+    gapV = 0;
+    gap = 0;
+    // console.log(gapV);
+  });
+  $(".verticalControlDown").mouseenter(function() {
+    gapV = 5;
+    gap = 0.1;
+    //  console.log(gapV);
+  });
+  $(".verticalControlDown").mouseout(function() {
+    gapV = 0;
+    gap = 0;
+    //console.log(gapV);
+  });
+});
+var mouseX = 0,
+  mouseY = 0;
+var xp = 0,
+  yp = 0;
+let xpb = 0,
+  ypb = 0;
+
+$(document).mousemove(function(e) {
+  mouseX = e.pageX - 15;
+  mouseY = e.pageY - 15;
 });
 
+setInterval(function() {
+  xp += (mouseX - xp) / 6;
+  yp += (mouseY - yp) / 6;
+  if (gap == 0) {
+    xpb = lerp(xpb, xp, 1);
+    ypb = lerp(ypb, yp, 1);
+  } else {
+    xpb += (xp - xpb) / 6 + gap;
+    ypb += (yp - ypb) / 6 + gapV;
+    // console.log(gapV);
+  }
 
-  var mouseX = 0,
-    mouseY = 0;
-  var xp = 0,
-    yp = 0;
-  let xpb = 0,
-      ypb = 0;
-  
-  
-  $(document).mousemove(function(e) {
-    mouseX = e.pageX - 15;
-    mouseY = e.pageY - 15;
-  });
-
-  setInterval(function() {
-    xp += (mouseX - xp) / 6;
-    yp += (mouseY - yp) / 6;
-    if(gap == 0){
-      xpb = lerp (xpb, xp, 1);
-      ypb = lerp (ypb, yp, 1);;
-    }else{
-      xpb += (xp - xpb) / 6 + gap;
-    ypb += (yp - ypb) / 6 ;
-    }
-   
-    $("#circleA").css({ left: xp + "px", top: yp + "px" });
-    $("#circleB").css({ left: xpb + "px", top: ypb + "px" });
-  }, 20);
-function lerp (start, end, amt){
-  return (1-amt)*start+amt*end
+  $("#circleA").css({ left: xp + "px", top: yp + "px" });
+  $("#circleB").css({ left: xpb + "px", top: ypb + "px" });
+}, 20);
+function lerp(start, end, amt) {
+  return (1 - amt) * start + amt * end;
 }
-//
-//let canvas;
-//let d = 0;
-//let space = 25;
-//let radius = 20;
-//let radiusMin = 15;
-//function windowResized() {
-//  resizeCanvas(windowWidth, windowHeight);
-
-//}
-//function setup() {
-//  canvas = createCanvas(windowWidth, windowHeight);
-//  frameRate(50);
-//}
-//function draw() {
-//  clear();
-//  noStroke();
-//
-//  fill(0);
-//  if (mouseIsPressed && radius > radiusMin) {
-//    radius -= 2;
-//  } else {
-//    radius = lerp(radius, 20, 0.08);
-//  }
-//  ellipse(mouseX, mouseY, 20);
-//  push();
-//  if (next == 1) {
-//    d = lerp(d, space, 0.1);
-//  } else if (next == 2) {
-//    d = lerp(d, -space, 0.1);
-//  } else if (next == 0) {
-//    d = lerp(d, 0, 0.08);
-//  }
-//  translate(d, 0);
-//  //fill(0);
-//  ellipse(pmouseX, pmouseY, radius + 5);
-//  pop();
-//}
-//
